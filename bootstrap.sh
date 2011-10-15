3004996bdaa47c795a02d7b82d5246825a74bdae
Francesc Esplugas <contact@francescesplugas.com>
2011-10-15 21:31:51 +0300

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,6 +7,4 @@ sudo apt-get -y install build-essential
 sudo apt-get -y install git-core
 
 # Extras for RubyGems and Rails:
-sudo apt-get -y install zlib1g-dev
-sudo apt-get -y install libssl-dev libsqlite3-dev
-sudo apt-get -y install libreadline5-dev
+sudo apt-get -y install zlib1g-dev libssl-dev libreadline5-dev
