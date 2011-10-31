26a8789e353621ecc6d85a6061669a03b0930f95
Francesc Esplugas <francesc.esplugas@gmail.com>
2011-10-31 09:02:48 +0100

    Do only what's really needed

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
-# Update, upgrade and install development tools:
+# Update, install development tools and git:
 sudo apt-get update
-sudo apt-get -y upgrade
 sudo apt-get -y install build-essential
-sudo apt-get -y install git-core curl
+sudo apt-get -y install git-core
 
 # Extras for RubyGems and Rails:
 sudo apt-get -y install zlib1g-dev libssl-dev
